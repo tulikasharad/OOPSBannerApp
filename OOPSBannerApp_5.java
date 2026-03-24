64159eb6b620b7ead1a0fa715a76bbbf7cfa8cff
tulikasharad <tulikasharad15@gmail.com>
2026-03-24 23:16:36 +0530

    Added Java program

---
+++ OOPSBannerApp_5.java
@@ -0,0 +1,16 @@
+/*
+ * @author: Tulika Sharad
+ * @version: 1.0
+ */
+
+public class OOPSBannerApp_5 {
+    public static void main(String[] args) {
+        System.out.println(String.join("\n", new String[] {
+            "  ***   ***  ****  **** ",
+            " *   * *   * *   * *  ",
+            " *   * *   * ****  *** ",
+            " *   * *   * *       * ",
+            "  ***   ***  *    **** "
+        }));
+    }
+}
